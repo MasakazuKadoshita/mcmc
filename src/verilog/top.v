f7bd9a5817d70edbb0ca38cf183f9ef84320780a
Masakazu Kadoshita <mkadoshita@gmail.com>
2017-09-27 13:57:18 +0900

    refs#13 study embedding submodule

---
+++ src/verilog/top.v
@@ -0,0 +1,22 @@
+module TOP(
+  input IN_A, // in  : Input A
+  input IN_B, // in  : Input B
+  input IN_C, // in  : Input C
+  output O,   // out : Output
+);
+
+  wire Z;
+  
+  MY_XOR2 U1(
+    .IN_A (IN_A),
+    .IN_B (IN_B),
+    .O    (Z)
+  );
+  
+  MY_XOR2 U2(
+    .IN_A (IN_C),
+    .IN_B (Z),
+    .O    (O)
+  );
+
+endmodule
