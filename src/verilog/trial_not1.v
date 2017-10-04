4a09c1cf35cc80f78dceb87896b9341077698907
Masakazu Kadoshita <mkadoshita@gmail.com>
2017-10-05 06:01:25 +0900

    refs#008 trial_not1 verilog file

---
+++ src/verilog/trial_not1.v
@@ -0,0 +1,15 @@
+/* trial module */
+/* 1 inputs NOT */
+
+module TRIAL_NOT1(
+  input  IN  ,  //  input
+  output O      //  output
+);
+
+//-----
+// NOT 
+//-----
+  
+  assign O = ~IN;
+  
+endmodule
